90d308cbb8daa00d47afb005b4f57dd32e9382ab
llwyd1 <llwydmodes@gmail.com>
2021-08-31 20:16:58 +0200

    Added task 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
+ * followed by a new line
+ */
+void print_alphabet_x10(void)
+{
+	char cha;
+	int i;
+
+	for (i = 1; i <= 10; i++)
+	{
+		for (cha = 'a'; cha <= 'z'; cha++)
+		{
+			_putchar(cha);
+		}
+		_putchar('\n');
+	}
+}
