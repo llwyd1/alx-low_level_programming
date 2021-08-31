eab37841511a502066c71c37d787453faea8c31b
llwyd1 <llwydmodes@gmail.com>
2021-08-31 19:49:31 +0200

    Added task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_alphabet - prints the alphabet in lowercase, followed by a new line
+ * Return: 0 Always (success)
+ */
+void print_alphabet(void)
+{
+	char cha;
+
+	for (cha = 'a'; cha <= 'z'; cha++)
+	{
+		_putchar(cha);
+	}
+	_putchar('\n');
+	return (0);
+}
