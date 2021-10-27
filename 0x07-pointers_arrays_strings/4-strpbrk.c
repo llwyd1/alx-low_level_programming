1dead32d680bcb86bd2a589515de4987ef367c2c
llwyd1 <llwydmodes@gmail.com>
2021-10-27 12:56:33 +0200

    Added task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+
+char *_strpbrk(char *s, char *accept)
+{
+	int i, j;
+
+	for (i = 0; *s != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (*s == accept[j])
+			{
+				return (s);
+			}
+		}
+		s++;
+	}
+
+	return (NULL);
+}
