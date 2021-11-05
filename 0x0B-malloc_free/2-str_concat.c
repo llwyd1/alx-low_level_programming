f74c6f91f0c86bb704affa0dbe612f41e69e824c
llwyd1 <llwydmodes@gmail.com>
2021-11-05 15:25:44 +0200

    Added task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,4 +1,5 @@
 #include <stdlib.h>
+#include <stdio.h>
 #include "main.h"
 
 /**
@@ -12,10 +13,9 @@ char *str_concat(char *s1, char *s2)
 	char *s3;
 	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 
-	while (s1[len1] != '\0')
+	while (s1 && s1[len1])
 		len1++;
-
-	while (s2[len2] != '\0')
+	while (s2 && s2[len2])
 		len2++;
 
 	s3 = malloc(sizeof(char) * (len1 + len2 + 1));
@@ -42,7 +42,7 @@ char *str_concat(char *s1, char *s2)
 			i++;
 			j++;
 		}
-       	}
+	}
 	s3[i] = '\0';
 
 	return (s3);
