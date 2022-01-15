bc333a5c365c77d788d7252a08598ed76b7c4600
llwyd1 <llwydmodes@gmail.com>
2022-01-15 12:06:31 +0200

    Added task 4

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,40 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <string.h>
+/**
+ * new_dog - creates new dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Return: (on success) pointer to the new dog, otherwise NULL
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *dog;
+
+	dog = malloc(sizeof(dog_t));
+
+	if (dog == NULL)
+		return (NULL);
+
+	dog->name = malloc(sizeof(char) * (strlen(name) + 1));
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	dog->owner = malloc(sizeof(char) * (strlen(owner) + 1));
+	if (dog->owner == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+
+	strcpy(dog->name, name);
+	strcpy(dog->owner, owner);
+	dog->age = age;
+
+	return (dog);
+}
