04ceeb5ad32d6294b501766bbc922b8bd40b5557
llwyd1 <llwydmodes@gmail.com>
2022-05-17 12:14:28 +0200

    Added task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,17 +13,11 @@ size_t print_list(const list_t *h)
 	while (h)
 	{
 		if (!h->str)
-		{
 			printf("[0] (nil)\n");
-			h = h->next;
-			count++;
-		}
 		else
-		{
 			printf("[%u] (%s)\n", h->len, h->str);
-			count++;
-			h = h->next;
-		}
+		count++;
+		h = h->next;
 	}
 	return (count);
 }
