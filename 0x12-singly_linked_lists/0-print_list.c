1ada945693bfd3a312125f60ff6220ba04a27b7e
llwyd1 <llwydmodes@gmail.com>
2022-05-17 12:03:17 +0200

    Added task 0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * print_list - prints all elements of a list
+ * @h: is the pointer to the list to be printed
+ *
+ * Return: number of nodes
+ */
+size_t print_list(const list_t *h)
+{
+	size_t count = 0;
+
+	while (h)
+	{
+		if (!h->str)
+		{
+			printf("[0] (nil)\n");
+			h = h->next;
+			count++;
+		}
+		else
+		{
+			printf("[%u] (%s)\n", h->len, h->str);
+			count++;
+			h = h->next;
+		}
+	}
+	return (count);
+}
---
+++ 0x12-singly_linked_lists/README.md
@@ -0,0 +1,3 @@
+***This directory contains files for singly linked lists project***
+
+File 0-print_list.c - is a function that prints all the elements of a list_t list.<br>
