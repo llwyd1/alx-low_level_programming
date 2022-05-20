331a6ec413b7e8a3284b1133a1f57f9b38b14a43
llwyd1 <llwydmodes@gmail.com>
2022-05-20 09:39:11 +0200

    Added task 2

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_node - adds a node at the beginning of a list_t list
+ * @head: a double pointer to the list
+ * @str: the string to add in the new node
+ *
+ * Return: the address of the new element, or NULL if it failed
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *new = malloc(sizeof(list_t));
+	unsigned int len = 0;
+
+	while (str[len])
+		len++;
+
+	if (!new)
+		return (NULL);
+
+	new->str = strdup(str);
+	new->len = len;
+
+	new->next = *head;
+	*head = new;
+
+	return (*head);
+}
--- 0x12-singly_linked_lists/README.md
+++ 0x12-singly_linked_lists/README.md
@@ -3,3 +3,5 @@
 File 0-print_list.c - is a function that prints all the elements of a list_t list.<br>
 
 File 1-list_len.c - is a function that returns the number of elements of a list_t list <br>
+
+File 2-add_node.c - is a function that adds a new node at the beginning of a list_t list <br>
