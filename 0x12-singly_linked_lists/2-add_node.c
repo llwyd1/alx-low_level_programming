57d2a0e1739f54a421df14a647e5b9bfff872f13
llwyd1 <llwydmodes@gmail.com>
2022-05-24 11:28:48 +0200

    Added task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -12,17 +12,17 @@ list_t *add_node(list_t **head, const char *str)
 	list_t *new = malloc(sizeof(list_t));
 	unsigned int len = 0;
 
+	if (!new || !str)
+	{
+		free(new);
+		return (NULL);
+	}
+
 	while (str[len])
 		len++;
 
 	new->str = strdup(str);
 	new->len = len;
-
-	if ((!new->str) || (!new))
-	{
-		free(new);
-		return (NULL);
-	}
 	new->next = (*head);
 	(*head) = new;
 
