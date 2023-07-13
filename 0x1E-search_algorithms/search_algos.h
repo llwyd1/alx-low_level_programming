5eac32e06ca77d656c38aaaba48e11498973899f
Lloyd <llwydmodes@gmail.com>
2023-07-13 12:10:52 +0200

    Corrected header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -8,6 +8,6 @@
 /*Function prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
-int print_array(int *array, int low, int high);
+void print_array(int *array, size_t low, size_t high);
 
 #endif
