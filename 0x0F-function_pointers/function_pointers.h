1f3e47abd77f2684e4021dc6433bdc36d8d92f3d
Lloyd <llwydmodes@gmail.com>
2024-02-28 14:19:21 +0200

    Added task 0

---
+++ 0x0F-function_pointers/function_pointers.h
@@ -0,0 +1,11 @@
+#ifndef FUNCTION_POINTERS_H
+#define FUNCTION_POINTERS_H
+
+/**
+void print_name_as_is(char *name);
+void print_name_uppercase(char *name);
+*/
+
+void print_name(char *name, void (*f)(char *));
+
+#endif
