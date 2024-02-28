1b3ad34828a311db689eeb272d4c73edc9c0842c
Lloyd <llwydmodes@gmail.com>
2024-02-28 15:38:46 +0200

    Added task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - searches for an integer
+ * @array: array to search from
+ * @size: size of the array
+ * Return: index of the first element the integer occurs
+ */
+int int_index(int *array, int size, int (cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (i);
+	}
+	return (-1);
+}
